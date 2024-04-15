c3eb4b676dfdd2590750193dd1c6064d90f3b9a9
juanmontilva <montilva.juancarlos@gmail.com>
2024-04-14 23:25:57 -0400

    envio-theview

--- js/secondary.js
+++ js/secondary.js
@@ -16,6 +16,15 @@ window.addEventListener('scroll', () => {
 //     }
 // });
 
+// boton de contacto
+const textButtons = document.querySelectorAll('.contact__btn');
+
+
+textButtons.forEach(textButtons =>{
+    let text = textButtons.querySelector('p');
+
+    text.innerHTML = text.innerHTML.split('').map((character,index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join('')
+})
 
 
 
