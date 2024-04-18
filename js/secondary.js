9e181cd6ce1974781b1d321d5f494d670a224abd
juanmontilva <montilva.juancarlos@gmail.com>
2024-04-18 02:55:38 -0400

    act-new-theview

--- js/secondary.js
+++ js/secondary.js
@@ -48,3 +48,66 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 
+// =======ERROR DE CODIGO========
+// const nav = document.querySelector('.nav__links');
+// const openNavBtn = document.querySelector('#nav__toggle-open');
+// const closeNavBtn = document.querySelector('#nav__toggle-close');
+
+// const openNav = () =>{
+//     nav.style.display = 'flex';
+//     openNavBtn.style.display = 'none';
+//     closeNavBtn.style.display = 'inline-block';
+
+// }
+
+// openNavBtn.addEventListener('click', openNav);
+
+
+
+
+// const closeNav = () =>{
+//     nav.style.display = 'none';
+//     closeNavBtn.style.display = 'inline-block';
+//     closeNavBtn.style.display = 'none';
+
+// }
+
+// closeNavBtn.addEventListener('click', closeNav);
+
+// nav.querySelectorAll('li a').forEach(navLink =>{
+//     navLink.addEventListener('click', closeNav);
+// })
+
+
+
+
+// =====CODIGO CORREGIDO====
+const nav = document.querySelector('.nav__links');
+const openNavBtn = document.querySelector('#nav__toggle-open');
+const closeNavBtn = document.querySelector('#nav__toggle-close');
+
+const openNav = () => {
+    nav.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+}
+
+openNavBtn.addEventListener('click', openNav);
+
+const closeNav = () => {
+    nav.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+}
+
+closeNavBtn.addEventListener('click', closeNav);
+if(document.body.clientWidth < 1024){
+    nav.querySelectorAll('li a').forEach(navLink => {
+        navLink.addEventListener('click', closeNav);
+    })
+}
+
+
+
+
+
